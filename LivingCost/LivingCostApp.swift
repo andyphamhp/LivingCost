c8c9711781f6f477c63b679a4a03a3e1523b8eed
Andy Pham <anhpham6@asimtelecom.vn>
2023-07-10 10:49:30 +0700

    Initial Commit

---
+++ LivingCost/LivingCostApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivingCostApp.swift
+//  LivingCost
+//
+//  Created by Andy Pham on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LivingCostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
