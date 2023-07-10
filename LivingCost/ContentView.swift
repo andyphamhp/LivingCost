e9943b13932d3d2a2bc934ba29560b0477b5d0ac
Andy Pham <anhpham6@asimtelecom.vn>
2023-07-11 01:52:18 +0700

    Update layout

--- LivingCost/ContentView.swift
+++ LivingCost/ContentView.swift
@@ -10,40 +10,61 @@ import SwiftUI
 struct ContentView: View {
     
     struct Category: Identifiable {
-        
+
         enum CatType: String, CaseIterable {
             case electricity
             case water
             case setting
         }
-        
         let id = UUID()
         let type: CatType
+        
+        func symbolName() -> String {
+            switch type {
+            case .electricity:
+                return "electric_bolt"
+            case .water:
+                return "water"
+            case .setting:
+                return "settings"
+            }
+        }
     }
     
     private let categoryListEnum: [String] = {
         let allCase = Category.CatType.allCases
         return allCase.map(\.rawValue)
     }()
-    
+
     private let categoryList: [Category] = {
-        return Array<Category.CatType>([.electricity, .water]).map { type in
+        return Array<Category.CatType>([.electricity, .water, .setting]).map { type in
             Category(type: type)
         }
     }()
     
+    private func destination(name: String) -> some View {
+        return DetailView(category: name)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(name.capitalized)
+    }
+    
     var body: some View {
         NavigationView {
-            List(categoryListEnum, id: \.self) { category in
-                NavigationLink {
-                    DetailView(category: category)
-                        .navigationBarTitleDisplayMode(.inline)
-                        .navigationTitle(category.capitalized)
-                } label: {
-                    Text(category.capitalized)
+            List(categoryList) { list in
+                let name = list.type.rawValue.capitalized
+                NavigationLink(destination: destination(name: name)) {
+                    Image(list.symbolName())
+                    Text(name)
                 }
             }
-            .navigationTitle("Main")
+            .navigationTitle("Calculate")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink(destination: destination(name: "Setting")) {
+                        Image(systemName: "gear")
+                    }
+                }
+            }
         }
     }
 }
@@ -51,5 +72,29 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func debugModifier<T: View>(_ modifier: (Self) -> T) -> some View {
+        #if DEBUG
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+}
+
+extension View {
+    func debugBorder(_ color: Color = .red, width: CGFloat = 1) -> some View {
+        debugModifier {
+            $0.border(color, width: width)
+        }
+    }
+    
+    func debugBackground(_ color: Color = .red) -> some View {
+        debugModifier {
+            $0.background(color)
+        }
     }
 }
